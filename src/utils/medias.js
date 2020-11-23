55d0b4bd61531f9da87a514066181ae1c9950bd0
raywom@github.com <40425058+raywom@users.noreply.github.com>
2020-11-23 18:09:45 +0100

    234234

--- src/utils/medias.js
+++ src/utils/medias.js
@@ -2,7 +2,7 @@ export function getStrapiMedia(url) {
   // Check if URL is a local path
   if (url.startsWith('/')) {
     // Prepend Strapi address
-    const strapiUrl = process.env.GRIDSOME_STRAPI_URL || 'http://localhost:1337'
+    const strapiUrl = process.env.GRIDSOME_STRAPI_URL || 'https://nagima-site.herokuapp.com'
     return strapiUrl + url
   }
   // Otherwise return full URL
